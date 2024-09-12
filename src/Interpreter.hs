3afabb730c81663e66e1859a703e1b91667700c9
Ellis Gibbons <egibbons256@gmail.com>
2024-09-12 13:38:39 -0400

    Fix index function (again) and add deindex implementation

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -2,33 +2,28 @@ module Interpreter where
 
 import AST
 
-import Data.Text (Text)
-import Data.HashSet
 import Control.Monad.State
 import Data.List (elemIndex)
-import Data.Maybe (fromMaybe)
+
 
 -- Gives De Brujin indices to each variable
 index :: Expr -> State [Ident] Indexed
-index (Var var) = fromMaybe <$> freeVar <*> getVar
-    where
-        getVar :: State [Ident] (Maybe Indexed)
-        getVar = gets $ fmap Var . elemIndex var
-        freeVar :: State [Ident] Indexed
-        freeVar = do
+index (Var var) = do
+    maybeVar <- gets $ fmap Var . elemIndex var
+    case maybeVar of
+        Just v -> return v
+        Nothing -> do
             i <- gets length
             modify (++ [var])
             return $ Var i
 index (Apply expr1 expr2) = Apply <$> index expr1 <*> index expr2
 index (Lambda var expr) = Lambda var <$> (modify (var :) *> index expr <* modify tail)
 
-index' :: Expr -> Indexed
-index' = flip evalState [] . index
-
 -- Recover original variable names
 deindex :: Indexed -> State [Ident] Expr
-deindex = undefined
-
+deindex (Var i) = gets $ Var . (!! i)
+deindex (Apply expr1 expr2) = Apply <$> deindex expr1 <*> deindex expr2
+deindex (Lambda var expr) = Lambda var <$> (modify (var :) *> deindex expr <* modify tail)
 
 -- Replaces variables with an expression
 subst :: Int -> Indexed -> Indexed -> Indexed
@@ -44,4 +39,4 @@ eval (Lambda var expr) = Lambda var $ eval expr
 eval x = x
 
 interpret :: Expr -> Expr
-interpret = undefined
+interpret = flip evalState [] . (deindex . eval <=< index)
