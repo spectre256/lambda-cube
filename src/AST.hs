8870becbd86c388262794443ffef5cb1059ccfb5
Ellis Gibbons <egibbons256@gmail.com>
2024-09-12 23:55:42 -0400

    Add REPL

--- src/AST.hs
+++ src/AST.hs
@@ -1,6 +1,6 @@
 module AST where
 
-import Data.Text (Text)
+import Data.Text (Text, unpack)
 
 type Ident = Text
 
@@ -11,4 +11,26 @@ data Expr' a b
     = Var a
     | Apply (Expr' a b) (Expr' a b)
     | Lambda b (Expr' a b)
-    deriving (Show, Eq)
+    deriving (Eq)
+
+
+showParens :: Show a => a -> String
+showParens x = '(' : show x ++ ")"
+
+showTermE :: Expr -> String
+showTermE e@(Var _) = show e
+showTermE e = showParens e
+
+showTermI :: Indexed -> String
+showTermI e@(Var _) = show e
+showTermI e = showParens e
+
+instance Show Expr where
+    show (Var var) = unpack var
+    show (Apply expr1 expr2) = showTermE expr1 ++ " " ++ showTermE expr2
+    show (Lambda var expr) = "λ" ++ unpack var ++ ". " ++ show expr
+
+instance Show Indexed where
+    show (Var i) = show i
+    show (Apply expr1 expr2) = showTermI expr1 ++ " " ++ showTermI expr2
+    show (Lambda _ expr) = "λ. " ++ show expr
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Parser
+import Interpreter
+
+import Data.Text (pack)
+import System.Console.Haskeline
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = runInputT defaultSettings loop
+    where
+        loop = do
+            minput <- getInputLine "λ> "
+            case minput of
+                Nothing -> loop
+                Just line -> accept (pack line) >> loop
+        accept line = case parse line of
+            Nothing -> outputStrLn "Bad syntax, idiot"
+            Just ast -> outputStrLn . show $ interpret ast
